c03bbd10cf7202f5e952d8d334687b6b646787b1
rhcp <rhcp011235@gmail.com>
2012-01-07 01:59:56 -0500

    Fix up some stuff

--- full_toro.mk
+++ full_toro.mk
@@ -25,12 +25,45 @@
 PRODUCT_PACKAGES := \
     Camera \
     Gallery \
+    Superuser \
+    Superuser.apk \
+    su
 
 #if we do this after the full_base_telephony is included some of these don't get picked up..
 PRODUCT_COPY_FILES += \
-    device/samsung/toro/bcmdhd.cal:system/etc/wifi/bcmdhd.cal \
+ device/samsung/toro/bcmdhd.cal:system/etc/wifi/bcmdhd.cal \
     device/sample/etc/apns-conf_verizon.xml:system/etc/apns-conf.xml \
-    frameworks/base/data/etc/android.hardware.telephony.cdma.xml:system/etc/permissions/android.hardware.telephony.cdma.xml
+    device/samsung/toro/prebuilt/bootanimation.zip:system/media/bootanimation.zip \
+    frameworks/base/data/etc/android.hardware.telephony.cdma.xml:system/etc/permissions/android.hardware.telephony.cdma.xml \
+    frameworks/base/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.com.google.clientidbase=android-verizon \
+    ro.com.google.locationfeatures=1 \
+    ro.url.legal=http://www.google.com/intl/%s/mobile/android/basic/phone-legal.html \
+    ro.url.legal.android_privacy=http://www.google.com/intl/%s/mobile/android/basic/privacy.html \
+    ro.setupwizard.mode=OPTIONAL \
+    ro.cdma.home.operator.numeric=310004 \
+    ro.cdma.home.operator.alpha=Verizon \
+    ro.cdma.homesystem=64,65,76,77,78,79,80,81,82,83 \
+    ro.cdma.data_retry_config=default_randomization=2000,0,0,120000,180000,540000,960000 \
+    ro.gsm.2nd_data_retry_config=max_retries=infinite,default_randomization=2000,0,0,80000,125000,485000,905000 \
+    ro.cdma.otaspnumschema=SELC,1,80,99 \
+    ro.com.android.dataroaming=false \
+    ro.com.android.wifi-watchlist=GoogleGuest \
+    ro.error.receiver.system.apps=com.google.android.feedback \
+    ro.setupwizard.enterprise_mode=1 \
+    ro.config.ringtone=Girtab.ogg \
+    ro.config.notification_sound=Proxima.ogg \
+    ro.config.alarm_alert=Cesium.ogg
+
+# Override some toro specific build information
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=mysid \
+    BUILD_ID=ICL53F \
+    BUILD_DISPLAY_ID=IML74K \
+	BUILD_FINGERPRINT="google/mysid/toro:4.0.2/ICL53F/235179:user/release-keys" \
+	PRIVATE_BUILD_DESC="mysid-user 4.0.2 ICL53F 235179 release-keys"
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
@@ -42,5 +75,7 @@ $(call inherit-product, device/samsung/toro/device.mk)
 # Set those variables here to overwrite the inherited values.
 PRODUCT_NAME := full_toro
 PRODUCT_DEVICE := toro
-PRODUCT_BRAND := Android
-PRODUCT_MODEL := Full AOSP on Toro
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Galaxy Nexus
+PRODUCT_MANUFACTURER := Samsung
+
