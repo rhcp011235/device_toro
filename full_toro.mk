c17da16b5b4b9a07a41cc08f3f26c73aa9d9160f
rhcp <rhcp011235@gmail.com>
2012-01-15 16:11:39 -0500

    Fix for version

--- full_toro.mk
+++ full_toro.mk
@@ -62,9 +62,16 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=mysid \
     BUILD_ID=ICL53F \
     BUILD_DISPLAY_ID=IML74K \
-	BUILD_FINGERPRINT="google/mysid/toro:4.0.2/ICL53F/235179:user/release-keys" \
-	PRIVATE_BUILD_DESC="mysid-user 4.0.2 ICL53F 235179 release-keys"
-
+    BUILD_FINGERPRINT="google/mysid/toro:4.0.2/ICL53F/235179:user/release-keys" \
+    PRIVATE_BUILD_DESC="mysid-user 4.0.2 ICL53F 235179 release-keys" \
+    TARGET_BUILD_TYPE=user \
+    BUILD_VERSION_TAGS=release-keys \
+    USER=rhcp \
+    BUILD_HOST=RHCP-BUILDBOX \
+    PRODUCT_DEFAULT_LANGUAGE=en \
+    PRODUCT_DEFAULT_REGION=US \
+    PLATFORM_VERSION=4.0.3
+   
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # This is where we'd set a backup provider if we had one
